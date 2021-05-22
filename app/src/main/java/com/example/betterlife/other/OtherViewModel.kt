1d55277414fa018456c8ad28f9982844b4af4e8f
scolley31 <scolley31@gmail.com>
2021-05-23 03:21:55 +0800

    can realtime update in main page and other page

--- app/src/main/java/com/example/betterlife/other/OtherViewModel.kt
+++ app/src/main/java/com/example/betterlife/other/OtherViewModel.kt
@@ -54,8 +54,6 @@ class OtherViewModel(private val repository: PlanRepository) : ViewModel()  {
         Logger.i("------------------------------------")
 
         getOtherPlanResult()
-
-//        setMockData()
     }
 
     fun addToOtherTask(plan: Plan) {
@@ -63,9 +61,8 @@ class OtherViewModel(private val repository: PlanRepository) : ViewModel()  {
         coroutineScope.launch {
 
                 val result = repository.addToOtherTask("Scolley",plan.id)
-                Log.d("result","result = $result")
-                getOtherPlanResult()
-            
+                _otherPlan.value = _otherPlan.value
+
         }
 
     }
@@ -78,8 +75,6 @@ class OtherViewModel(private val repository: PlanRepository) : ViewModel()  {
             _status.value = LoadApiStatus.LOADING
 
             val result = repository.getOtherPlanResult()
-
-            Log.d("test","result = $result")
 
             _otherPlan.value = when (result) {
                 is Result.Success -> {
@@ -103,56 +98,7 @@ class OtherViewModel(private val repository: PlanRepository) : ViewModel()  {
                     null
                 }
             }
-            Log.d("test","_otherPlan = ${_otherPlan.value}")
             _refreshStatus.value = false
         }
     }
-
-
-//    fun setMockData() {
-//
-//        var mock = mutableListOf<Plan>()
-//        mock.run {
-//            add(Plan("1", "跑步", "運動", "", "2200-0412",
-//                    listOf("Wayne","Sean"),"",4000,
-//                    listOf(Completed("1","Scolley",true,300,"2200-0412")
-//                            , Completed("2","Wayne",true,300,"2200-0412")
-//                            , Completed("3","Sean",true,200,"2200-0412"))
-//                    ,300
-//                    ,null))
-//
-//            add(Plan("2", "跳繩", "運動", "", "2200-0412",
-//                    listOf("Wayne","Sean"),"",2000,
-//                    listOf(Completed("1","Scolley",true,200,"2200-0412")
-//                            , Completed("2","Wayne",true,300,"2200-0412")
-//                            , Completed("3","Sean",true,100,"2200-0412"))
-//                    ,300
-//                    ,null))
-//
-//            add(Plan("3", "打籃球", "運動", "", "2200-0412",
-//                    listOf("Wayne","Sean"),"",10000,
-//                    listOf(Completed("1","Scolley",true,100,"2200-0412")
-//                            , Completed("2","Wayne",true,500,"2200-0412")
-//                            , Completed("3","Sean",true,300,"2200-0412"))
-//                    ,300
-//                    ,null))
-//
-//            add(Plan("4", "打羽球", "運動", "", "2200-0412",
-//                    listOf("Wayne","Sean"),"",5000,
-//                    listOf(Completed("1","Scolley",true,300,"2200-0412")
-//                            , Completed("2","Wayne",true,200,"2200-0412")
-//                            , Completed("3","Sean",true,300,"2200-0412"))
-//                    ,300
-//                    ,null))
-//
-//            add(Plan("5", "散步", "運動", "", "2200-0412",
-//                    listOf("Wayne","Sean"),"",3000,
-//                    listOf(Completed("1","Scolley",true,300,"2200-0412")
-//                            , Completed("2","Wayne",true,300,"2200-0412")
-//                            , Completed("3","Sean",true,300,"2200-0412"))
-//                    ,300
-//                    ,null))
-//        }
-//        _otherPlan.value = mock
-//    }
 }
